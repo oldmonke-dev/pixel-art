fd39d09a673065673d102e5741096c259c8f55f4
leygen02 <adityacsahu02@gmail.com>
2024-11-28 22:08:59 +0530

    Porting to Angular

---
+++ pixel-art/src/app/canvas/canvas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas',
+  imports: [],
+  templateUrl: './canvas.component.html',
+  styleUrl: './canvas.component.css'
+})
+export class CanvasComponent {
+
+  
+
+}
